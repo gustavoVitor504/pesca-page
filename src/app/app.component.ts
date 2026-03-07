ae321857f0fc9f1658e67daa25b09bb7b46801a3
Gustavo Vitor <guvitorfn504@gmail.com>
2026-03-07 00:47:31 -0300

    chore: add app root and routing setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponentComponent } from './layout/Header/header-component/header-component.component';
+import { SubHeaderComponent } from './layout/sub-header/sub-header/sub-header.component';
+import { ProductPageComponent } from "./pages/product-page/product-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponentComponent, SubHeaderComponent, ProductPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProductPageComponent
+  },
+  {
+    path: 'carrinho',
+    component: CartPageComponent
+  }
+];
